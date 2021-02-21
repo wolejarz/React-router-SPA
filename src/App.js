7ad444ee27f436255643485316c8773309ee811e
Wojciech Olejarz <wojciech.olejarz@onet.eu>
2021-02-21 14:42:03 +0100

    react-router added, App component wrapped by BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,27 +1,40 @@
 import React, { Component } from "react";
+import { BrowserRouter, Link } from "react-router-dom";
 import "./App.css";
 
 class App extends Component {
   state = {};
   render() {
     return (
-      <div>
-        <header>
-          <nav>
-            <ul>
-              <li>
-                <a href="/"></a>
-              </li>
-              <li>
-                <a href="/news"></a>
-              </li>
-              <li>
-                <a href="/content"></a>
-              </li>
-            </ul>
-          </nav>
-        </header>
-      </div>
+      <BrowserRouter>
+        <div>
+          <header>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/">Start</Link>
+                </li>
+                <li>
+                  <Link to="/news">News</Link>
+                </li>
+                <li>
+                  <Link to="/contact">Contact</Link>
+                </li>
+
+                {/* <li>
+                  <a href="/">Start</a>
+                </li>
+                <li>
+                  <a href="/news">News</a>
+                </li>
+                <li>
+                  <a href="/contact">Contact</a>
+                </li> */}
+              </ul>
+            </nav>
+          </header>
+        </div>
+      </BrowserRouter>
     );
   }
 }
